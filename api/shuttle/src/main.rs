32425857dfea18076057b13686519176eacf45eb
hirotake111 <hirotake111@users.noreply.github.com>
2023-08-17 07:11:11 +0900

    add database connection

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_shared_db::Postgres;
+use sqlx::PgPool;
 
 #[get("/")]
 async fn hello_world() -> &'static str {
@@ -8,6 +10,7 @@ async fn hello_world() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn actix_web(
+    #[Postgres] pool: PgPool,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
