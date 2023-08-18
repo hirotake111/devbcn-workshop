ae63f8c58e445f945a9ac2f1193388c7e4433bff
hirotake111 <hirotake111@users.noreply.github.com>
2023-08-19 00:49:00 +0900

    add film repository trait

---
+++ api/lib/src/film_repository/mod.rs
@@ -0,0 +1,14 @@
+pub type FilmError = String;
+pub type FilmResult<T> = Result<T, FilmError>;
+
+use shared::models::{CreateFilm, Film};
+use uuid::Uuid;
+
+#[async_trait::async_trait]
+pub trait FilmRepository: Send + Sync + 'static {
+    async fn get_films(&self) -> FilmResult<Vec<Film>>;
+    async fn get_film(&self, id: &Uuid) -> FilmResult<Film>;
+    async fn create_film(&self, film: &CreateFilm) -> FilmResult<Film>;
+    async fn update_film(&self, film: &Film) -> FilmResult<Uuid>;
+    async fn delete_film(&self, id: &Uuid) -> FilmResult<Uuid>;
+}
