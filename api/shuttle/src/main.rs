206d3479a023d02ac69d0506dce1edb47bf8ca99
hirotake111 <hirotake111@users.noreply.github.com>
2023-08-20 14:03:40 +0900

    serve static files

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use actix_web::web;
 use api_lib::film_repository::PostgresFilmRepository;
 use shuttle_actix_web::ShuttleActixWeb;
@@ -6,6 +8,7 @@ use shuttle_shared_db::Postgres;
 #[shuttle_runtime::main]
 async fn actix_web(
     #[Postgres] pool: sqlx::PgPool,
+    #[shuttle_static_folder::StaticFolder(folder = "static")] static_folder: PathBuf,
 ) -> ShuttleActixWeb<impl FnOnce(&mut web::ServiceConfig) + Send + Clone + 'static> {
     let film_repository = PostgresFilmRepository::new(pool);
     // let film_repository: web::Data<Box<dyn FilmRepository>> =
@@ -17,9 +20,20 @@ async fn actix_web(
     // .map_err(CustomError::new)?;
 
     let config = move |cfg: &mut web::ServiceConfig| {
-        cfg.app_data(film_repository)
-            .configure(api_lib::health::service)
-            .configure(api_lib::films::service::<PostgresFilmRepository>);
+        // cfg.app_data(film_repository)
+        // .configure(api_lib::health::service)
+        // .configure(api_lib::films::service::<PostgresFilmRepository>);
+        cfg.service(
+            web::scope("/api")
+                .app_data(film_repository)
+                .configure(api_lib::health::service)
+                .configure(api_lib::films::service::<PostgresFilmRepository>),
+        )
+        .service(
+            actix_files::Files::new("/", static_folder)
+                .show_files_listing()
+                .index_file("index.html"),
+        );
     };
 
     Ok(config.into())
